4f27dca7aefe49f7cd0e929b212440c6a1c79db9
Saeed Altaf <saeedaltaf@gmail.com>
2019-10-17 11:43:32 -0400

    Updates

---
+++ precache-manifest.b654c58951ca4b282add6e2c9eb72635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a1d22ec9b2eb05cb9575580abe3b41",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "b1ece9a449cca3b26532",
+    "url": "/google-book-search/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4e470f97fb3aa2743ab1",
+    "url": "/google-book-search/static/js/2.86da77eb.chunk.js"
+  },
+  {
+    "revision": "b1ece9a449cca3b26532",
+    "url": "/google-book-search/static/js/main.64844dcf.chunk.js"
+  },
+  {
+    "revision": "36ced401d8544d423c97",
+    "url": "/google-book-search/static/js/runtime-main.15b61ddf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/google-book-search/static/media/logo.25bf045c.svg"
+  }
+]);
